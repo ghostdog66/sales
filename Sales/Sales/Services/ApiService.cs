dcc4a375713ea4b088993bf0ade52ecdb55a73c5
Ghostdog66 <ppen_llamas@hotmail.com>
2018-08-16 18:26:40 +0200

    First Commit

---
+++ Sales/Sales/Services/ApiService.cs
@@ -0,0 +1,55 @@
+﻿
+
+namespace Sales.Services
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ApiService
+    {
+        public async Task<Response> GetList<T>(string urlBase,string prefix,string controller)
+        {
+            try
+            {
+                var client =new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var url = $"{prefix}{controller}";
+                var response = await client.GetAsync(url);
+                var answer = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSucces = false,
+                        Message = answer,
+
+                    };
+                }
+
+                var list = JsonConvert.DeserializeObject<T>(answer);
+                return new Response
+                {
+                    IsSucces = true,
+                    Result = list,
+                };
+
+            }
+            catch (Exception ex)
+            {
+
+                return new Response
+                {
+                    IsSucces = false,
+                    Message = ex.Message,
+
+                };
+            }
+
+        }
+    }
+}
---
+++ Sales/Sales/Services/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace Sales.Services
+{
+    public class Response
+    {
+        public bool IsSucces { get; set; }
+
+        public string Message { get; set; }
+
+        public object Result { get; set; }
+    }
+}
